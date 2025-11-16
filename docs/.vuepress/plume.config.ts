1928bee79fe26227fa75bbd6dfa0010b718eb77b
maodou <2723374657@qq.com>
2025-11-16 21:23:33 +0800

    修改配置

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -18,7 +18,7 @@ import collections from './collections'
  * @see https://theme-plume.vuejs.press/config/basic/
  */
 export default defineThemeConfig({
-  logo: 'https://theme-plume.vuejs.press/plume.png',
+  logo: 'https://lqq-picture.oss-cn-chengdu.aliyuncs.com/img/avatar.jpg',
 
   appearance: true,  // 配置 深色模式
 
